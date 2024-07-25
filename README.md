05cd7d7581b18598d2f3cc017c4d90ade6c7635d
zmj <3296523@qq.com>
2024-07-25 17:48:45 +0800

    centos_init.sh

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # tools
 
-wget -N https://raw.githubusercontent.com/hanjun806/tools/main/read_user.sh && bash read_user.sh ssh_reader 
+```
+wget -N https://raw.githubusercontent.com/hanjun806/tools/main/centos_init.sh && bash centos_init.sh
+
+wget -N https://raw.githubusercontent.com/hanjun806/tools/main/read_user.sh && bash read_user.sh ssh_reader xxxx
+
+sed -i '/^#PasswordAuthentication yes/s/^#//' /etc/ssh/sshd_config
+echo "AllowUsers root ssh_reader" >> /etc/ssh/sshd_config
+systemctl restart sshd.service
+
+```
---
+++ centos_init.sh
@@ -0,0 +1,42 @@
+#/bin/bash
+
+# 安装wget等
+yum -y install lrzsz wget
+
+# 安装docker
+curl -fsSL https://get.docker.com | sh
+systemctl enable docker.service
+systemctl start docker
+
+# 同步时间
+yum install ntpdate -y
+ntpdate -u time1.google.com
+
+sudo yum update ca-certificates -y
+
+systemctl restart docker
+
+# 安装x-ui
+mkdir -p /mnt/x-ui && cd /mnt/x-ui
+
+docker run -itd --network=host \
+-v $PWD/db/:/etc/x-ui/ \
+-v /mnt/data/cert/:/root/cert/ \
+--name x-ui --restart=unless-stopped \
+enwaiax/x-ui:alpha-zh
+
+systemctl start firewalld
+
+# 打开防火墙，动态端口处理
+firewall-cmd --permanent --add-port=22/tcp
+firewall-cmd --permanent --add-port=34521/tcp
+firewall-cmd --permanent --add-forward-port=port=40000-41000:proto=tcp:toport=5001
+firewall-cmd --permanent --add-forward-port=port=41000-42000:proto=tcp:toport=5002
+firewall-cmd --permanent --add-forward-port=port=42000-43000:proto=tcp:toport=5011
+firewall-cmd --permanent --add-forward-port=port=43000-44000:proto=tcp:toport=5012
+firewall-cmd --permanent --add-rich-rule='rule family="ipv4" source address="45.78.17.247" accept'
+firewall-cmd --reload
+firewall-cmd --list-all
+systemctl restart firewalld
+systemctl restart docker
+
